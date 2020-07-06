7d9e94c537bd18f8bb96acd99243710924a4ebc1
beckag1 <40666908+beckag1@users.noreply.github.com>
2020-07-06 11:53:44 +0100

    Update serious-calculations-test.js

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -1,8 +1,8 @@
 var assert = require('assert'),
-    vows = require('vows'),
+                  vows = require('vows'),
     seriousCalculations = require('../');
 
-vows.describe('serious-calculations').addBatch({
+    vows.describe('serious-calculations').addBatch({
   'when performing serious calculations': {
     topic: function() {
       return seriousCalculations.performSeriousCalculations(4);
@@ -10,6 +10,6 @@ vows.describe('serious-calculations').addBatch({
     'result should be valid': function (topic) {
       assert.isNumber(topic);
       assert.equal(topic, 8);
-    }
+                }
   }
 }).export(module);
